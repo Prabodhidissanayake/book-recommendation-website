4238a8d64ae851c1e72f5faf30950326ad299104
Prabodhi <prabodhi38@gmail.com>
2023-03-14 15:55:38 +0100

    adding react components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './reset.css';
 import './App.css';
+import BookCard from './components/bookCard/BookCard';
 
 function App() {
-  const [data, setData] = useState("");
-
-  // useEffect(() => {
-  //   axios.get('http://localhost:3000/')
-  //     .then(response => {
-  //       setData(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
-  useEffect(() => {
-    axios.get('http://localhost:3000/books')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-
   return (
   <>
-    <h1>Book recommendation Website</h1>
-    {data}
+  <header>
+    <nav>
+    <h1 className='nav__title nav__title-top'>BookLover</h1>
+    <h3 className='nav__title nav__title-bottom'>Book Recommendation Website</h3>
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">My Preferences</a></li>
+    </ul>
+    </nav>
+  </header>
+  <section className='search-bar'>
+  <form id="form"> 
+  <input type="search" id="query" name="q" placeholder="Search..."/>
+  <select name="" id="">
+  <option value="">All</option>
+    <option value="">Romance</option>
+    <option value="">History</option>
+    <option value="">Science Fiction</option>
+    <option value="">Fiction</option>
+  </select>
+  <button>Search</button>
+</form>
+  </section>
+  <main className='container'>
+    <BookCard/>
+  </main>
+
   </>
 );
 }
