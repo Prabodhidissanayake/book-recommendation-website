07c0d9df3258bf745f7b1a819650f662367224ea
Prabodhi <prabodhi38@gmail.com>
2023-03-14 18:42:31 +0100

    book basic view

--- frontend/src/components/BookDetails/BookDetails.tsx
+++ frontend/src/components/BookDetails/BookDetails.tsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function BookDetails() {
+  const [bookInfo, setBookInfo] = useState<any>(null);
+  const searchParams = new URLSearchParams(useLocation().search);
+  const id = searchParams.get('id');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchBookInfo() {
+      try {
+        const response = await fetch(`http://localhost:3000/api/books/${id}`);
+        if (response.status === 204) {
+          setTimeout(() => {
+            navigate('/');
+          }, 3000);
+          throw new Error('No book found');
+        } else if (response.ok) {
+          const data = await response.json();
+          setBookInfo(data);
+        } else {
+          throw new Error('Something went wrong');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchBookInfo();
+  }, [id, navigate]);
+
   return (
-    <div>BookDetails</div>
-  )
+    <div>
+      {bookInfo ? (
+        <div>
+          <h2>{bookInfo.title}</h2>
+          <p>{bookInfo.author}</p>
+          <p>{bookInfo.description}</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
