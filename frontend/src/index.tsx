5a621217c2eb2a30528de1d516628792b5d85f81
Prabodhi <prabodhi38@gmail.com>
2023-03-14 19:43:25 +0100

    book create review done

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BookDetails from './components/BookDetails/BookDetails';
+import Preferences from './components/Preferences/Preferences';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +16,7 @@ root.render(
       <Routes>
         <Route  path="/" Component={App} />
         <Route  path="/book" Component={BookDetails} />
+        <Route  path="/preferences" Component={Preferences} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
