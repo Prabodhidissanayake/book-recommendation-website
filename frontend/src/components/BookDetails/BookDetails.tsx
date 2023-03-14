1db5ee19c45576425d25c168bd828f454a17eead
Prabodhi <prabodhi38@gmail.com>
2023-03-14 19:13:11 +0100

    reviews update

--- frontend/src/components/BookDetails/BookDetails.tsx
+++ frontend/src/components/BookDetails/BookDetails.tsx
@@ -3,8 +3,9 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function BookDetails() {
   const [bookInfo, setBookInfo] = useState<any>(null);
-  const [rating, setRating] = useState<number>(0);
-  const [comment, setComment] = useState<string>('');
+  const [rating, setRating] = useState<number | undefined>();
+  const [comment, setComment] = useState<string | undefined>();
+  const [hasCommented, setHasCommented] = useState(false);
   const searchParams = new URLSearchParams(useLocation().search);
   const id = searchParams.get('id');
   const navigate = useNavigate();
@@ -28,31 +29,45 @@ export default function BookDetails() {
         console.error(error);
       }
     }
+
+    async function fetchReview() {
+      try {
+        const response = await fetch(`http://localhost:3000/api/reviews/${id}`);
+        if (response.status === 204) {
+          return;
+        } else if (response.ok) {
+          const data = await response.json();
+          setHasCommented(true);
+          setRating(data.rating);
+          setComment(data.comment);
+        } else {
+          throw new Error('Something went wrong');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
     fetchBookInfo();
+    fetchReview();
   }, [id, navigate]);
 
-  async function submitReview() {
-    if (rating === 0) {
-      alert('Please enter a rating');
-      return;
-    }
-    const body = {
-      bookId: id,
-      rating,
-      comment,
-    };
+  async function handleCommentSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/api/reviews', {
-        method: 'POST',
+      const response = await fetch(`http://localhost:3000/api/reviews/${id}`, {
+        method: hasCommented ? 'PATCH' : 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(body),
+        body: JSON.stringify({
+          rating,
+          comment,
+        }),
       });
       if (response.ok) {
-        alert('Review submitted successfully');
-        setRating(0);
-        setComment('');
+        setHasCommented(true);
+        alert('Comment submitted!');
       } else {
         throw new Error('Something went wrong');
       }
@@ -68,29 +83,32 @@ export default function BookDetails() {
           <h2>{bookInfo.title}</h2>
           <p>{bookInfo.author}</p>
           <p>{bookInfo.description}</p>
-          <div>
-            <label htmlFor="rating">Rating:</label>
-            <input
-              type="number"
-              id="rating"
-              name="rating"
-              value={rating}
-              onChange={(e) => setRating(parseInt(e.target.value))}
-            />
-          </div>
-          <div>
-            <label htmlFor="comment">Comment:</label>
-            <textarea
-              id="comment"
-              name="comment"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-            />
-          </div>
-          <button onClick={submitReview}>Submit Review</button>
+          <form onSubmit={handleCommentSubmit}>
+            <label>
+              Rating:
+              <input
+                type="number"
+                min="1"
+                max="10"
+                value={rating}
+                onChange={(event) => setRating(parseInt(event.target.value))}
+                required
+              />
+            </label>
+            <label>
+              Comment:
+              <textarea
+                value={comment}
+                onChange={(event) => setComment(event.target.value)}
+              />
+            </label>
+            <button type="submit">
+              {hasCommented ? 'Update Comment' : 'Submit Comment'}
+            </button>
+          </form>
         </div>
       ) : (
-        <p>No book found. Redirecting to home page...</p>
+        <p>Loading...</p>
       )}
     </div>
   );
